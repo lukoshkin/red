b7158b69d908acdfc910d163278e3fe0896d14bb
lukoshkin <lukoshkin@phystech.edu>
2021-07-13 18:20:46 +0300

    Add options to save base image and strip sfu zip

--- setup.sh
+++ setup.sh
@@ -2,25 +2,20 @@
 
 [[ "$@" = *=* ]] && { echo '= is invalid sign in arguments'; exit 1; }
 
+source utils/utils.sh
+
 
 uid=$(id -u)
 gid=$(id -g)
 
 ncores=4
 examples="$PWD/examples"
+strip_sfu_zip_flag=false
+save_base_img_flag=false
 
 
-help_msg () {
-  echo 'Usage: ./setup.sh [opts] [img_name]'
-  echo -e '\nOptions:'
-  printf '  %-16s Mount a folder with examples.\n' '-e'
-  printf "  %-16s Specify user IDs inside the container.\n" '-i'
-  printf '  %-16s Set the number of cores for building the base image.\n' '-n'
-  echo
-}
-
-params=$(getopt -o e:,i:,n: --name "$0" -- "$@")
-[[ $? -ne 0 ]] && { help_msg; exit; }
+params=$(getopt -o e:,i:,n:,sb,sz --name "$0" -- "$@")
+[[ $? -ne 0 ]] && { utils::help_msg; exit; }
 eval set -- $params
 unset params
 
@@ -33,6 +28,8 @@ do
   case $1 in
     -n) ncores=$2; shift 2 ;;
     -e) examples=$2; shift 2 ;;
+    -sz) strip_sfu_zip_flag=true; shift ;;
+    -sb) save_base_img_flag=true; shift ;;
     -i)
       uid=$(cut -d ',' -f1 <<< $2);
       gid=$(cut -d ',' -f2 <<< $2);
@@ -45,33 +42,31 @@ shift
 [[ $# -gt 1 ]] && { echo More than one positional argument given!; exit 1; }
 [[ -n $1 ]] && img=$1 || img=red
 
+base=$img
+save_base_img_flag && base+=':base'
 
-find_string () {
-  local sel=$1
-  shift
-
-  for str in $@
-  do
-    [[ $str = $sel ]] && return 0
-  done
-  return 1
-}
 
 containers=$(docker ps -a --format '{{.Names}}')
-find_string $img $containers \
+utils::find_string $img $containers \
   && { echo container \"$img\" already exists; exit 1; }
 
 
 mkdir -p vimmed
 
-docker build --build-arg UID=$uid --build-arg GID=$gid --build-arg N_CORES=$ncores -t $img . \
+docker build --build-arg UID=$uid --build-arg GID=$gid --build-arg N_CORES=$ncores -t $base . \
   && wget -O vimmed/Dockerfile https://raw.githubusercontent.com/lukoshkin/evangelist/master/Dockerfile \
-  && docker build --build-arg IMG_NAME=$img -t $img vimmed/ \
+  && docker build --build-arg IMG_NAME=$base -t $img vimmed/ \
   && rm -rf vimmed || :
 
 [[ $? -ne 0 ]] && { echo Problems with building the images; exit 1; }
-unzip -n PoreFlow*.zip -d project 2> /dev/null \
-  && chmod +x project/bin/poremesh
+
+if strip_sfu_zip_flag
+then
+  utils::strip_zip_2dir PoreFlow*.zip project
+else
+  unzip -n PoreFlow*.zip -d project 2> /dev/null || echo PoreFlow*.zip not found.
+  chmod +x project/bin/poremesh || echo PoreFlow*.zip has a different structure than expected.
+fi
 
 echo
 [[ -d project  && -d $examples ]] \
---
+++ utils/utils.sh
@@ -0,0 +1,28 @@
+utils::help_msg () {
+  echo 'Usage: ./setup.sh [opts] [img_name]'
+  echo -e '\nOptions:'
+  printf '  %-16s Mount a folder with examples.\n' '-e'
+  printf "  %-16s Specify user IDs inside the container.\n" '-i'
+  printf '  %-16s Set the number of cores for building the base image.\n' '-n'
+  echo
+}
+
+
+utils::find_string () {
+  local sel=$1
+  shift
+
+  for str in $@
+  do
+    [[ $str = $sel ]] && return 0
+  done
+  return 1
+}
+
+utils::strip_zip_2dir () {
+  mkdir $2
+  unzip -d $2 $1 2> /dev/null
+  local dir=$(ls $2)
+  mv $2/*/* $2
+  rmdir $2/$dir
+}
